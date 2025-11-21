1ea435f46fa85eec45ad0710dfeea9a0d8b1b574
SherifaSayed <sherifasayedabrahem@gmail.com>
2025-11-21 07:23:17 +0200

    q8 script

--- main_q6.js
+++ main_q6.js
@@ -1,4 +1,20 @@
-let names =["a","ab","abc","abcd"];
+let weekNum=5;
 
-let LenghthsOfNames= names.map(name=>name.length);
-console.log(LenghthsOfNames);
+
+switch(weekNum)
+{
+    case 1:console.log("SunDay")
+     break;
+      case 2:console.log("Monday")
+     break;
+      case 3:console.log("Tuesday")
+     break;
+      case 4:console.log("Wednesday")
+     break;
+      case 5:console.log("Thursday")
+     break;
+      case 6:console.log("Friday")
+     break;
+      case 7:console.log("Saturday")
+     break;
+}
---
+++ main_q8.js
@@ -0,0 +1,9 @@
+function DivisibleBY3OR5(n1)
+{
+    if(n1%3==0&&n1%5==0)
+        console.log("devisible by both");
+    else 
+        console.log("Not Devisible by 3 or 5")
+}
+
+DivisibleBY3OR5(15);

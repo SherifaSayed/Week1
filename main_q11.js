99fee4fa0afcf4a86d25c1a227bcd418782bc3f5
SherifaSayed <sherifasayedabrahem@gmail.com>
2025-11-21 20:42:26 +0200

    q2 script modifide + q11 script

---
+++ main_q11.js
@@ -0,0 +1,7 @@
+function Sum(data={})
+{
+    const {n1=0, n2=0, n3=0,n4=0,n5=0,n6=0}= data
+    return n1+n2+n3+n4+n5+n6;
+}
+
+console.log(Sum({n1:10,n2:20,n5:5}));
--- main_q2.js
+++ main_q2.js
@@ -1,10 +1,8 @@
-const readline = require('readline');
-const r1= readline.createInterface
-(
-    {input : process.stdin , output: process.stdout}
-);
-r1.question("write a value ", (answer)=>{
-    if (answer==0||answer==false||answer=="")
-        console.log("invalid");
-        
-})
+function checkFalsy(value) {
+  if (!value) {
+    return "Invalid";
+  }
+  return value;
+}
+
+console.log(checkFalsy(0))

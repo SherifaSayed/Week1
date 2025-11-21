f991d1f79e149845ee4472c0a09213ca54705322
SherifaSayed <sherifasayedabrahem@gmail.com>
2025-11-21 07:10:50 +0200

    q5 script

---
+++ main_q5.js
@@ -0,0 +1,20 @@
+let weekNum=5;
+
+
+switch(weekNum)
+{
+    case 1:console.log("SunDay")
+     break;
+      case 2:console.log("Monday")
+     break;
+      case 3:console.log("Tuesday")
+     break;
+      case 4:console.log("Wednesday")
+     break;
+      case 5:console.log("Thursday")
+     break;
+      case 6:console.log("Friday")
+     break;
+      case 7:console.log("Saturday")
+     break;
+}

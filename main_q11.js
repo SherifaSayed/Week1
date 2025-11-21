cb413f634bb70f91b8588862f970c2110e8f3e8b
SherifaSayed <sherifasayedabrahem@gmail.com>
2025-11-21 20:43:08 +0200

    q11 script new comment

--- main_q11.js
+++ main_q11.js
@@ -1,3 +1,6 @@
+
+
+
 function Sum(data={})
 {
     const {n1=0, n2=0, n3=0,n4=0,n5=0,n6=0}= data
@@ -5,3 +8,4 @@ function Sum(data={})
 }
 
 console.log(Sum({n1:10,n2:20,n5:5}));
+// I have a question here 

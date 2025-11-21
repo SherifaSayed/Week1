4c7c100d6f31b8d4c9488c22ebc399c6d52c47ac
SherifaSayed <sherifasayedabrahem@gmail.com>
2025-11-21 07:15:49 +0200

    q6 script

--- main_q4.js
+++ main_q4.js
@@ -1,4 +1,4 @@
  let nums=[ 1,2,3,4,5];
- let num =(nums.filter(even=>{return even%2==0}));
+ let num =nums.filter(even=>{return even%2==0});
  console.log(num);
  
---
+++ main_q6.js
@@ -0,0 +1,4 @@
+let names =["a","ab","abc","abcd"];
+
+let LenghthsOfNames= names.map(name=>name.length);
+console.log(LenghthsOfNames);
